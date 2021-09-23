66b00c1ace85a153c344ba56f44e458621cb91c8
Raynder <raynder4@gmail.com>
2021-09-23 15:26:11 +0000

    criando script para o menu

--- js/js.js
+++ js/js.js
@@ -21,3 +21,17 @@ $(document).ready(function() {
     });
 });
 // FIm do script
+
+// script de abrir e fechar menu
+function mnu(){
+    mnu = document.querySelector('.rodape');
+    mnu.style.width = '50px';
+    mnu.style.height = '50px';
+    fas = document.querySelectorAll('.menu');
+    fas[0].getElementsByTagName('i')[0].style.fontSize = '0pt';
+    fas[1].getElementsByTagName('i')[0].style.fontSize = '0pt';
+    fas[2].getElementsByTagName('i')[0].style.fontSize = '0pt';
+    fas[3].getElementsByTagName('i')[0].style.fontSize = '0pt';
+    fas[4].getElementsByTagName('i')[0].style.fontSize = '0pt';
+}
+// fim do script do menu
